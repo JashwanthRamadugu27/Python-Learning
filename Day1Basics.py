17f5eb3fcf62a4d4d4119450869abd6548062c0a
Jashwanth Ramadugu <jashwanthramadugu27@gmail.com>
2026-09-13 22:39:36 +0530

    Add files via upload

---
+++ Day1Basics.py
@@ -0,0 +1,13 @@
+print("NAMASKAR 🛕🙏🏻, ASSALAM ALAIKUM 🤲🏻🕌, AMEN ✝️⛪")
+
+print("Jashwanth started learning Python programming — Day 1")
+
+print("Why am I learning Python?")
+print("Because Python is widely used in Machine Learning and Data Science.")
+
+print(" Get a good job, build a good career, stay close to my family,Get a good wife")
+print("and build a happy and meaningful life. 😇")
+
+print("Success is God-given, so be humble, stay kind, and help everyone. 🫱🏻‍🫲🏻😇")
+
+print(27 + 6 + 2007)   
